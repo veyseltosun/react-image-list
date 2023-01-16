84c361020b70ad9e11eb6e3ef37f8b89992c76f1
veyseltosun <postalarim1923@gmail.com>
2023-01-16 03:31:46 +0300

    first commit...

---
+++ src/components/SearchInput.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+class SearchInput extends React.Component {
+    render() {
+        return (
+            <div className="ui segment">
+                <form className="ui form">
+                    <div className="field">
+                        <div className="ui massive icon input">
+                        
+                           <input className="" type="text" name="text" placeholder="search ..." />
+                           <i className="search icon"></i>
+
+                        </div>
+
+                    </div>
+                </form>
+            </div>
+        )
+    }
+}
+
+export default SearchInput;
