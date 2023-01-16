a216dcb97dc2bf3847bb80c9cb5ebc54719c8a52
veyseltosun <postalarim1923@gmail.com>
2023-01-16 20:46:00 +0300

    ..

--- src/components/App.js
+++ src/components/App.js
@@ -7,8 +7,9 @@ import SearchInput from "./SearchInput";
 
 class App extends React.Component {
 
-    onSearchSubmit = (entry) => {
-        axios.get(`https://pixabay.com/api/?key=32890479-9c105d6129c482faf9ed98a32&q=${entry}&image_type=photo`)
+     async onSearchSubmit (entry)  {
+      const response =  await axios.get(`https://pixabay.com/api/?key=32890479-9c105d6129c482faf9ed98a32&q=${entry}&image_type=photo`)
+      console.log(response.data.hits)
 
     }
     
@@ -16,7 +17,7 @@ class App extends React.Component {
     render() {
 
         return (
-            <div className="ui container " style={{ marginTop: "30px" }}>
+            <div className="ui container" style={{ marginTop: "30px" }}>
 
                 <SearchInput onSearchSubmit={this.onSearchSubmit} />
             </div>
