9d85c19a208730537d28e5bef7dd8c6bc6daa80d
veyseltosun <postalarim1923@gmail.com>
2023-01-16 23:50:37 +0300

    ...

--- src/components/ImageList.jsx
+++ src/components/ImageList.jsx
@@ -2,14 +2,46 @@ import React from "react";
 
 const ImageList = (props) => {
 
-    const images = props.images.map( image =>{
-        return <img key={image.id} src={image.webformatURL} />
-    });
+   const {images} = props
 
     console.log(props);
     return (
-        <div>
-            {images}
+        <div style={{display:"flex", flexDirection:"column", alignItems:"center", }}> {
+            images?.map((image) => {
+                const{id, webformatURL, comments, user, tags, views} = image;
+                return (
+                    <div className="ui link cards" key={id} >
+                        <div className="card">
+                            <div className="image">
+                               <img src={webformatURL} />
+                            </div>
+                            <div className="content">
+                                <div className="header">User : {user}</div>
+                                <div className="description" >
+                                   Tags: {tags}
+                                 </div>
+
+                            </div>
+                            <div className="extra content">
+                                <span className="right floated" >
+                                    Views {views}
+                                </span>
+                                <span >
+                                    <i className="user icon"></i>
+                                    Comments {comments}
+                                </span>
+                            </div>
+                        </div>
+                    </div>
+
+                )
+            })
+
+
+        }
+
+
+
         </div>
     )
 }
